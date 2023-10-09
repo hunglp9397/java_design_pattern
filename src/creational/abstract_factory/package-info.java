e3ca06f3785fc42042fb99c815d2313e7df96f7b
hunglp9397 <lephihung0997@gmail.com>
2023-10-09 23:50:54 +0700

    init abstract factory method

---
+++ src/creational/abstract_factory/package-info.java
@@ -0,0 +1,22 @@
+package creational.abstract_factory;
+
+/* ------------------------------- ABSTRACT FACTORY-------------------------
+
+
+1. Định nghĩa
+- Là một design pattern, tạo ra một Super-factory dùng để tạo ra các factory khác
+- Là một pattern cao cấp hơn của Factory Method
+
+
+2. Kiến trúc
+- Abstract Factory : Là một Abstract Class hoặc Interface chứa phương thức để tạo ra đối tượng Abstract
+- Abstract Product : Là một Abstract class hoặc Interface để định nghi đối tượng Abstract
+- Concreate Factory : Xây dựng các phương thức để tạo đối tượng cụ thể
+- Product : Xây dựng các đối tượng cụ thể, implement các phương thức ở Abstract Product
+- Client : Đối tượng sử dụng Abstract Factory và Abstract Product
+
+
+
+
+
+ */
